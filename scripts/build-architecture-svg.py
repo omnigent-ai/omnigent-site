1588bebe02d970e323290793e0638a1e3acc164b
lisancao <lisanatashacao@gmail.com>
2026-06-11 15:00:00 -0700

    design(home): use "+ more" under sandbox providers
    Co-authored-by: Isaac

--- scripts/build-architecture-svg.py
+++ scripts/build-architecture-svg.py
@@ -177,7 +177,7 @@ parts.append(text(cxin, sb_y + 22, 14.5, 600, FG, "middle").format("Sandboxing")
 logo_cy = sb_y + 54
 parts.append(img(cxin - 50, logo_cy, 34, 30, data_uri("public/logos/runners/modal.svg")))
 parts.append(img(cxin + 22, logo_cy, 86, 28, data_uri("public/logos/runners/daytona.png")))
-parts.append(text(cxin, sb_y + 88, 17, 700, FG_SOFT, "middle").format("…"))
+parts.append(text(cxin, sb_y + 86, 12.5, 600, FG_SOFT, "middle").format("+ more"))
 
 # Reliability box
 rb_y, rb_h = sb_y + sb_h + 12, 32
